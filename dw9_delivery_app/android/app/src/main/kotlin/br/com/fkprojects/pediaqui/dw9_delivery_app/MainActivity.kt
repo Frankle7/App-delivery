a65452dfe2cf502199bb5d9c7ba2665fb8fe4bfe
Isfrankle Souza <isfrankle.silva@grupopan.com>
2023-02-03 14:11:48 -0300

    feat(AppAndroid): preparing the Android application development environment

---
+++ dw9_delivery_app/android/app/src/main/kotlin/br/com/fkprojects/pediaqui/dw9_delivery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.fkprojects.pediaqui.dw9_delivery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dw9_delivery_app/lib/app/core/ui/theme/theme_config.dart
@@ -0,0 +1,41 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+import '../styles/colors_app.dart';
+
+class ThemeConfig {
+  ThemeConfig._();
+  static final _defaultInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(7),
+    borderSide: BorderSide(color: Colors.grey[400]!),
+  );
+
+  static final theme = ThemeData(
+      scaffoldBackgroundColor: Colors.black,
+      appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 3, 3, 3),
+          elevation: 0,
+          centerTitle: true,
+          iconTheme: IconThemeData(color: Colors.white)),
+      primaryColor: ColorsApp.i.primary,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: ColorsApp.i.primary,
+        primary: ColorsApp.i.primary,
+        secondary: ColorsApp.i.secondary,
+      ),
+      elevatedButtonTheme:
+          ElevatedButtonThemeData(style: AppStyles.i.primaryButton),
+      inputDecorationTheme: InputDecorationTheme(
+        fillColor: Colors.black,
+        filled: true,
+        isDense: true,
+        contentPadding: const EdgeInsets.all(13),
+        border: _defaultInputBorder,
+        enabledBorder: _defaultInputBorder,
+        focusedBorder: _defaultInputBorder,
+        labelStyle: TextStyles.i.textRegular.copyWith(color: Colors.white),
+        errorStyle: TextStyles.i.textRegular.copyWith(color: Colors.redAccent)
+      ),
+      );
+}
