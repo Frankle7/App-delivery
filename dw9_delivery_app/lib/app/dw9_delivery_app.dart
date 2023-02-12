2e2250d772f79c49fec8cbd0d4249d123a583461
Isfrankle Souza <isfrankle.silva@grupopan.com>
2023-02-11 22:58:21 -0300

    feat(registerPage): creating registration screen

--- dw9_delivery_app/lib/app/dw9_delivery_app.dart
+++ dw9_delivery_app/lib/app/dw9_delivery_app.dart
@@ -1,6 +1,7 @@
 import 'package:dw9_delivery_app/app/core/provider/application_binding.dart';
 import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/auth/login/login_page.dart';
+import 'package:dw9_delivery_app/app/pages/auth/register/register_page.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_router.dart';
 import 'package:dw9_delivery_app/app/pages/products_detail/products_detail_router.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
@@ -20,6 +21,7 @@ class Dw9DeliveryApp extends StatelessWidget {
           '/home': (context) =>  HomeRouter.page,
           '/productDetail':(context) => ProductsDetailRouter.page,
           '/auth/login':(context) => const LoginPage(), 
+          '/auth/register':(context) => const RegisterPage(), 
         },
       ),
     );
--- dw9_delivery_app/lib/app/pages/auth/login/login_page.dart
+++ dw9_delivery_app/lib/app/pages/auth/login/login_page.dart
@@ -4,87 +4,87 @@ import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-
-  const LoginPage({ super.key });
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: DeliveryAppbar(),
-           body: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Form(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Login', style: context.textStyles.textTitle,
-                        ),
-                        const SizedBox(
-                           height: 30,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'E-mail',
-                            ),
-                        ),
-
-                        const SizedBox(
-                           height: 30,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Senha',
-                            ),
-                        ),
-                        const SizedBox(
-                           height: 50,
-                        ),
-                         Center(
-                          child: DeliveryButton(
-                            width: double.infinity,
-                            label: 'ENTRAR', 
-                            onPressed: () { },
-                          ),
-                        )
-                        ],
-                        ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: DeliveryAppbar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Login',
+                      style: context.textStyles.textTitle,
                     ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'E-mail',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Senha',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Center(
+                      child: DeliveryButton(
+                        width: double.infinity,
+                        label: 'ENTRAR',
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
                 ),
               ),
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Não possui conta?', 
-                        style: context.textStyles.textBold.
-                        copyWith(color: Colors.white,),
-                        ),
-                        TextButton(
-                          onPressed: (){}, 
-                          child: const Text('Cadastre-se'),
-                        ),
-                      ]
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Não possui conta?',
+                    style: context.textStyles.textBold.copyWith(
+                      color: Colors.white,
                     ),
-                    ),
-                ),
-              )
-            ],
-           ),
-       );
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/auth/register');
+                    },
+                    child: const Text('Cadastre-se'),
+                  ),
+                ]),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
